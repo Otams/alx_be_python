22d17882740a562179fbaa703a6f87c21639d418
Otams <triumphotamiri@gmail.com>
2025-10-26 16:08:51 +0100

    Added the rectangle_area.py, hours_to_seconds.py and future_age_calculator.py files

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,10 @@
+# future_age_calculator.py
+
+# Prompt the user to input their current age
+current_age = int(input("How old are you? "))
+
+# Calculate the user's age in 2050
+future_age = current_age + 27  # Since 2050 - 2023 = 27
+
+# Print the result
+print("In 2050, you will be", future_age, "years old.")
---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,10 @@
+# hours_to_seconds.py
+
+# Define the given variable
+hours = 2  # Number of hours to convert
+
+# Calculate the number of seconds (1 hour = 3600 seconds)
+seconds = hours * 3600
+
+# Print the result
+print(hours, "hour(s) is", seconds, "seconds.")
